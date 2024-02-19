bfc2f0fa0e648bb8516b5f734a2bc8be087dfffc
Minh2024 <160540334+Minh2024@users.noreply.github.com>
2024-02-19 21:31:00 +0100

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem!!
+# ![2-title.png](attachment:Image%2019-02-2024%20at%2019.28.jpeg)
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# ![3-intro.png](attachment:Image%2019-02-2024%20at%2019.39.jpeg)
+
+# **Objectives:**
+# - List popular languages for Data Science
+# - List of popular libraries for Data Science
+# - List of popular Data Science Tools
+# ![10-objectives.png](attachment:Image%2019-02-2024%20at%2021.05.jpeg)
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQl
+# ![4-dslanguages.png](attachment:Image%2019-02-2024%20at%2019.46.jpeg)
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. ggplot 2
+# 2. Vegas
+# 3. Big DL
+# ![5-dslibraries.png](attachment:Image%2019-02-2024%20at%2019.58.jpeg)
+
+# Data Science Tools
+# ---
+# Pandas
+# ---
+# Hadoop
+# ---
+# Spark
+# ---
+# ![6-dstools.png](attachment:Image%2019-02-2024%20at%2019.55.jpeg)
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# ![7-introarithmetic](attachment:Image%2019-02-2024%20at%2020.00.jpeg)
+
+# This a simple arithmetic expression to multiply then add integers
+# ![8-multiplyandaddintegers.png](attachment:Image%2019-02-2024%20at%2020.11.jpeg)
+
+# In[8]:
+
+
+(3*4)+5
+
+
+# This will convert 200 to minutes by diving by 60
+# ![9-hourstominutes.png](attachment:Image%2019-02-2024%20at%2020.16.jpeg)
+
+# In[14]:
+
+
+200 // 60
+
+
+# ## Author 
+# Minh
+# ![11-authordetails.png](attachment:Image%2019-02-2024%20at%2021.06.jpeg)
